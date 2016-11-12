c2209e393caf535f3731c0ffc82881193f9d0548
Subhash Kumar <jietss2011@gmail.com>
2016-11-12 12:10:00 +0530

    Initial Commit

---
+++ app/models/house.rb
@@ -0,0 +1,5 @@
+class House < ActiveRecord::Base
+	has_many :floors
+	has_attached_file :image, styles: { medium: "300x300", thumb: "100x100" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+end
